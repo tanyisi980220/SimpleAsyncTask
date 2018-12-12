5dd62b451e0c83fa1a88afa2d0f0258f5c29464a
anreh <anurehka@gmail.com>
2018-12-12 14:15:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/simpleasynctask/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.taruc.simpleasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // The TextView where we will show results
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView1);
+    }
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+
+        // Start the AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+}
